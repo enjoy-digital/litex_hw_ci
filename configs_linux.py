eb1c22a4dc60157437b1b193e9c8f1c2a30e9c40
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-20 21:27:26 +0100

    config_linux: Add notes/comments and prepare more configs.

--- configs_linux.py
+++ configs_linux.py
@@ -10,14 +10,65 @@ from litex_hw_ci import LiteXCIConfig
 
 # LiteX CI Config Definitions ----------------------------------------------------------------------
 
+# Notes:
+# Tested configs?:
+# - VexRiscv-SMP with 1, 2, 4 cores?
+# - VexRiscv-SMP with FPU?
+# - NaxRiscv 32-bit with FPU?
+# - NaxRiscv 64-bit with FPU?
+
 litex_ci_configs = {
-    "arty:vexriscv-linux" : LiteXCIConfig(
+    # Digilent Arty running VexRiscv-SMP with:
+    # - 1 Core.
+    # - 8KB ICache (2-Ways).
+    # - 8KB DCache (2-Ways).
+    # - Coherent DMA.
+    # - Ethernet 100Mbps.
+    # - SPI-SDCard.
+    # - USB-Host.
+    "arty:vexriscv-linux-1-core" : LiteXCIConfig(
         target  = "digilent_arty",
         command = "--sys-clk-freq 100e6 \
-        --cpu-type=vexriscv_smp --cpu-variant=linux \
+        --cpu-type=vexriscv_smp --cpu-count=1 --cpu-variant=linux \
         --dcache-width=64 --dcache-size=8192 --dcache-ways=2 \
         --icache-width=64 --icache-size=8192 --icache-ways=2 \
         --dtlb-size=6 --with-coherent-dma --bus-bursting \
+        --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.100 \
+        --with-spi-sdcard \
+        --with-usb",
+        #tty     = "/dev/ttyUSB1",
+    ),
+    # Digilent Arty running VexRiscv-SMP with:
+    # - 2 Cores.
+    # - 8KB ICache (2-Ways).
+    # - 8KB DCache (2-Ways).
+    # - Coherent DMA.
+    # - Ethernet 100Mbps.
+    # - SPI-SDCard.
+    # - USB-Host.
+    "arty:vexriscv-linux-2-core" : LiteXCIConfig(
+        target  = "digilent_arty",
+        command = "--sys-clk-freq 100e6 \
+        --cpu-type=vexriscv_smp --cpu-count=1 --cpu-variant=linux \
+        --dcache-width=64 --dcache-size=8192 --dcache-ways=2 \
+        --icache-width=64 --icache-size=8192 --icache-ways=2 \
+        --dtlb-size=6 --with-coherent-dma --bus-bursting \
+        --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.100 \
+        --with-spi-sdcard \
+        --with-usb",
+        #tty     = "/dev/ttyUSB1",
+    ),
+    # Digilent Arty running NaxRiscv 32-bit with:
+    # - 1 Core.
+    # - Ethernet 100Mbps.
+    # - Coherent DMA.
+    # - Ethernet 100Mbps.
+    # - SPI-SDCard.
+    # - USB-Host.
+    "arty:naxriscv-linux-32-bit" : LiteXCIConfig(
+        target  = "digilent_arty",
+        command = "--sys-clk-freq 100e6 \
+        --cpu-type=naxriscv --scala-args='rvc=true,rvf=true,rvd=true' --with-fpu --with-rvc \
         --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.100 \
         --with-spi-sdcard \
         --with-usb",
