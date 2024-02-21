5ab228108837109b7dbe63ca67b21c3941bdf06a
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-21 09:15:09 +0100

    configs_linux: Simplify customizing local/repote_ip.

--- configs_linux.py
+++ configs_linux.py
@@ -17,6 +17,9 @@ from litex_hw_ci import LiteXCIConfig
 # - NaxRiscv 32-bit with FPU?
 # - NaxRiscv 64-bit with FPU?
 
+local_ip  = "192.168.1.50"
+remote_ip = "192.168.1.121"
+
 litex_ci_configs = {
     # Digilent Arty running VexRiscv-SMP with:
     # - 1 Core.
@@ -28,12 +31,12 @@ litex_ci_configs = {
     # - USB-Host through Machdyne PMOD on PMOD X formated as X.
     "arty:vexriscv-linux-1-core" : LiteXCIConfig(
         target  = "digilent_arty",
-        command = "--sys-clk-freq 100e6 \
+        command = f"--sys-clk-freq 100e6 \
         --cpu-type=vexriscv_smp --cpu-count=1 --cpu-variant=linux \
         --dcache-width=64 --dcache-size=8192 --dcache-ways=2 \
         --icache-width=64 --icache-size=8192 --icache-ways=2 \
         --dtlb-size=6 --with-coherent-dma --bus-bursting \
-        --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.121 \
+        --with-ethernet --eth-ip={local_ip} --remote-ip={remote_ip} \
         --with-spi-sdcard \
         --with-usb",
         post_command = "cd linux && python3 make.py --cpu-type=vexriscv --soc-json=../build_arty_vexriscv-linux-1-core/soc.json --linux-build --linux-generate-dtb --linux-prepare-tftp",
@@ -49,12 +52,12 @@ litex_ci_configs = {
     # - USB-Host through Machdyne PMOD on PMOD X formated as X.
     "arty:vexriscv-linux-2-core" : LiteXCIConfig(
         target  = "digilent_arty",
-        command = "--sys-clk-freq 100e6 \
+        command = f"--sys-clk-freq 100e6 \
         --cpu-type=vexriscv_smp --cpu-count=2 --cpu-variant=linux \
         --dcache-width=64 --dcache-size=8192 --dcache-ways=2 \
         --icache-width=64 --icache-size=8192 --icache-ways=2 \
         --dtlb-size=6 --with-coherent-dma --bus-bursting \
-        --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.100 \
+        --with-ethernet --eth-ip={local_ip} --remote-ip={remote_ip} \
         --with-spi-sdcard \
         --with-usb",
         post_command = "cd linux && python3 make.py --cpu-type=vexriscv --soc-json=../build_arty_vexriscv-linux-2-core/soc.json --linux-build --linux-generate-dtb --linux-prepare-tftp",
@@ -69,9 +72,9 @@ litex_ci_configs = {
     # - USB-Host through Machdyne PMOD on PMOD X formated as X.
     "arty:naxriscv-linux-32-bit" : LiteXCIConfig(
         target  = "digilent_arty",
-        command = "--sys-clk-freq 100e6 \
+        command = f"--sys-clk-freq 100e6 \
         --cpu-type=naxriscv --scala-args='rvc=true,rvf=true,rvd=true' --with-fpu --with-rvc \
-        --with-ethernet --eth-ip=192.168.1.50 --remote-ip=192.168.1.100 \
+        --with-ethernet --eth-ip={local_ip} --remote-ip={remote_ip} \
         --with-spi-sdcard \
         --with-usb",
         post_command = "cd linux && python3 make.py --cpu-type=vexriscv --soc-json=../build_arty_naxriscv-linux-32-bit/soc.json --linux-build --linux-generate-dtb --linux-prepare-tftp",
