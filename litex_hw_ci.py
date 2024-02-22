ff0189fa61e02839ad7791ed98d4c681985d9eab
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-22 10:11:03 +0100

    litex_hw_ci: Another cleanup pass.

--- litex_hw_ci.py
+++ litex_hw_ci.py
@@ -76,7 +76,7 @@ class LiteXCIConfig:
         self.name = name
 
     def perform_step(self, step_name, command, log_filename_suffix):
-        dst_dir = prepare_directory(self.name)
+        dst_dir  = prepare_directory(self.name)
         log_path = os.path.join(dst_dir, f"{log_filename_suffix}.rpt")
         if execute_command(command, log_path):
             return LiteXCIStatus.SUCCESS
@@ -92,8 +92,7 @@ class LiteXCIConfig:
     def software_build(self):
         if self.software_command == "":
             return LiteXCIStatus.NOT_RUN
-        command = self.software_command
-        return self.perform_step("build", command, "software_build")
+        return self.perform_step("build", self.software_command, "software_build")
 
     def load(self):
         command = f"python3 -m litex_boards.targets.{self.target} {self.gateware_command} \
@@ -101,22 +100,28 @@ class LiteXCIConfig:
         --load"
         return self.perform_step("load", command, "load")
 
-    def test(self, send="reboot\n"):
-        dst_dir = prepare_directory(self.name)
+    def test(self, reboot_command="reboot\n"):
+        dst_dir  = prepare_directory(self.name)
         log_path = os.path.join(dst_dir, "test.rpt")
         status = LiteXCIStatus.TEST_ERROR
+
+        # Open TTY and log file.
         with serial.Serial(self.tty, self.tty_baudrate, timeout=1) as ser, open(log_path, "w") as log_file:
-            ser.write(bytes(send, "utf-8"))
-            log_file.write(send)
             start_time = time.time()
-            accumulated_data = ""
+
+            # Reboot hardware.
+            ser.write(bytes(reboot_command, "utf-8"))
+
+            # Listen and log TTY until all keywords are found or timeout.
+            accumulated_keywords = ""
             while time.time() - start_time < self.test_timeout:
                 data = ser.read(256).decode('utf-8', errors='replace')
                 if data:
                     print(data, end='', flush=True)
                     log_file.write(data)
-                    accumulated_data += data
-                    if all(check in accumulated_data for check in self.test_keywords):
+                    accumulated_keywords += data
+                    # When all keywords are found, break with success status.
+                    if all(keyword in accumulated_keywords for keyword in self.test_keywords):
                         status = LiteXCIStatus.SUCCESS
                         break
         return status
@@ -129,33 +134,31 @@ def enum_to_str(enum_val):
         return enum_val.name
     return str(enum_val)
 
-# Modify the generate_html_report function to use enum_to_str for Enum conversion
 def generate_html_report(report, report_filename, steps, start_time, configs_file):
-    env = Environment(loader=FileSystemLoader(searchpath='./'))
+    env      = Environment(loader=FileSystemLoader(searchpath='./'))
     template = env.get_template('html/report_template.html')
 
-    # Convert Enum values to strings
+    # Convert Enum values to strings.
     for name, results in report.items():
         for step in steps:
             step_key = step.capitalize()
             if step_key in results:
                 results[step_key] = enum_to_str(results[step_key])
 
-    # Prepare summary information
-    tests_executed = sum(1 for results in report.values() if any(status != 'NOT_RUN' for status in results.values()))
-    total_seconds = sum(float(results.get('Duration', '0.00 seconds')[:-7]) for results in report.values())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    summary = f"<p>Build Start Time: {start_time} | Configs File: {configs_file}</p>"
+    # Prepare summary information.
+    tests_executed    = sum(1 for results in report.values() if any(status != 'NOT_RUN' for status in results.values()))
+    total_seconds     = sum(float(results.get('Duration', '0.00 seconds')[:-7]) for results in report.values())
+    hours, remainder  = divmod(total_seconds, 3600)
+    minutes, seconds  = divmod(remainder, 60)
+    summary  = f"<p>Build Start Time: {start_time} | Configs File: {configs_file}</p>"
     summary += f"<p>Number of tests executed: {tests_executed} | Total Duration: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds</p>"
 
-    # Render the template with the report data and summary
+    # Render the HTML template with the report data and summary.
     html_content = template.render(report=report, steps=steps, summary=summary)
 
-    # Write the rendered HTML to the report file
+    # Write the rendered HTML to the report file.
     with open(report_filename, 'w') as file:
         file.write(html_content)
-
 
 # LiteX CI Build/Test ------------------------------------------------------------------------------
 
@@ -170,10 +173,10 @@ def main():
     parser.add_argument("--list", action="store_true",     help="List all available configurations in file and exit.")
     args = parser.parse_args()
 
-    # Capture the start time
+    # Capture the start time.
     test_start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Import litex_ci_configs from the specified config file
+    # Import litex_ci_configs from the specified config file.
     try:
         config_module = __import__(args.configs_file.replace(".py", "").replace("/", "."), fromlist=[''])
         litex_ci_configs = config_module.litex_ci_configs
@@ -195,12 +198,14 @@ def main():
             return
         litex_ci_configs = {args.config: litex_ci_configs[args.config]}
 
-    steps = ['gateware_build', 'software_build', 'load', 'test']
+    steps  = ['gateware_build', 'software_build', 'load', 'test']
     report = {format_name(name): {step.capitalize(): LiteXCIStatus.NOT_RUN for step in steps} for name in litex_ci_configs}
 
+    # Generate empty HTML report.
     os.system("cp html/report_style.css ./")
     generate_html_report(report, args.report, steps, test_start_time, args.configs_file)
 
+    # Iterate on configurations and progressively update report.
     for name, config in litex_ci_configs.items():
         name = format_name(name)
         config.set_name(name)
@@ -212,7 +217,7 @@ def main():
                 break
         end_time = time.time()
         duration = end_time - start_time
-        report[name]['Time'] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
+        report[name]['Time']     = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
         report[name]['Duration'] = f"{duration:.2f} seconds"
 
         generate_html_report(report, args.report, steps, test_start_time, args.configs_file)
