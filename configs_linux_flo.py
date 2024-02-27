4d6dc551294c3440099cb04bd25c27c1430a829a
Florent Kermarrec <florent@enjoy-digital.fr>
2024-02-27 14:56:04 +0100

    config_linux_flo: Add NaxRiscv 32-bit and reduce sys_clk_freq to 100MHz now that possible.

--- configs_linux_flo.py
+++ configs_linux_flo.py
@@ -23,6 +23,24 @@ test_timeout = 60.0
 fixme = 32
 
 litex_ci_configs = {
+    # Acorn Baseboard Mini running NaxRiscv-32-bit with:
+    # - 1 Core.
+    # - FPU.
+    # - Coherent DMA.
+    # - Ethernet 1000Mbps.
+    "acorn_baseboard_mini_naxriscv_32_bit" : LiteXCIConfig(
+        target           = "litex_acorn_baseboard_mini",
+        gateware_command = f"--sys-clk-freq 100e6 \
+        --cpu-type=naxriscv --xlen 32 --scala-args='rvc=true,rvf=true,rvd=true' --with-rvc --with-fpu \
+        --with-coherent-dma \
+        --with-ethernet --eth-ip={local_ip} --remote-ip={remote_ip}",
+        software_command = f"cd linux && python3 make.py --cpu-type=naxriscv --xlen 32 --soc-json=../build_acorn_baseboard_mini_naxriscv_32_bit/soc.json --linux-clean --linux-build --linux-generate-dtb --linux-prepare-tftp",
+        setup_command    = "",
+        exit_command     = "",
+        tty              = "/dev/ttyUSB1",
+        test_keywords    = test_keywords,
+        test_timeout     = test_timeout,
+    ),
     # Acorn Baseboard Mini running NaxRiscv-64-bit with:
     # - 1 Core.
     # - FPU.
@@ -30,11 +48,11 @@ litex_ci_configs = {
     # - Ethernet 1000Mbps.
     "acorn_baseboard_mini_naxriscv_64_bit" : LiteXCIConfig(
         target           = "litex_acorn_baseboard_mini",
-        gateware_command = f"--sys-clk-freq 156.25e6 \
-        --cpu-type=naxriscv --xlen {fixme} --scala-args='rvc=true,rvf=true,rvd=true' --with-rvc --with-fpu \
+        gateware_command = f"--sys-clk-freq 100e6 \
+        --cpu-type=naxriscv --xlen 64 --scala-args='rvc=true,rvf=true,rvd=true' --with-rvc --with-fpu \
         --with-coherent-dma \
         --with-ethernet --eth-ip={local_ip} --remote-ip={remote_ip}",
-        software_command = f"cd linux && python3 make.py --cpu-type=naxriscv --xlen {fixme} --soc-json=../build_acorn_baseboard_mini_naxriscv_64_bit/soc.json --linux-clean --linux-build --linux-generate-dtb --linux-prepare-tftp",
+        software_command = f"cd linux && python3 make.py --cpu-type=naxriscv --xlen 64 --soc-json=../build_acorn_baseboard_mini_naxriscv_64_bit/soc.json --linux-clean --linux-build --linux-generate-dtb --linux-prepare-tftp",
         setup_command    = "",
         exit_command     = "",
         tty              = "/dev/ttyUSB1",
